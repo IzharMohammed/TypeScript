7b4a3fffd45359e0b1e61f8af754de378076d8bb
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-08-08 13:08:08 +0530

    Added types

--- Basics/hkirat.ts
+++ Basics/hkirat.ts
@@ -38,7 +38,7 @@ function isLegal(user: User) {
 }
 
 //2. Implementing interfaces
-// Interfaces have another special property. You can implement interfaces as a class but not in types
+// Interfaces have another special property. You can implement interfaces as a class but not in types and can be used in reusability 
 interface Person {
     name: string
     age: number;
@@ -55,4 +55,76 @@ class employee implements Person {
     greet(): void {
         console.log('greeting !!!');
     }
+}
+
+class manager implements Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.age = age;
+        this.name = name;
+    }
+    greet(): void {
+        console.log('greeting !!!');
+    }
+}
+
+
+// Types 
+// Very similar to interfaces , types let you aggregate data together.
+type User1 = {
+    firstName : string,
+    lastName : string,
+    age : number
+}
+
+// #1 1. Unions
+// Let’s say you want to print the id of a user, which can be a number or a string.
+// You can not do this using interfaces
+
+/* Method - 1  */
+// function printId(id : (string | number)){
+//     console.log('ID :- ',id);
+// }
+
+/* Method - 2 using types  */
+type ID = string | number;
+
+function printId(id : ID){
+    console.log('ID :- ',id);
+}
+printId(10);
+printId('izhar');
+// printId(true); //Error
+
+/*
+2. Intersection
+What if you want to create a type that has every property of multiple types/ interfaces
+You can not do this using interfaces
+*/
+
+type employee1 = {
+    name : string;
+    startDate : Date;
+}
+
+type manager1 = {
+    name : string,
+    department : string
+}
+
+/* Method -1  */
+// type TeamLead = {
+//     name : string;
+//     startDate : Date;
+//     department : string
+// }
+
+/* Method -2 using types but we cant use it in interfaces */
+type TeamLead = employee1 & manager1;
+
+const teamLead : TeamLead = {
+    name : 'kak',
+    startDate : new Date(),
+    department : 'SDE'
 }
